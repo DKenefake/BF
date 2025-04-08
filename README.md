25f373ffabe023c452895afad9915cfee65641a6
Dustin Kenefake <Dustin.Kenefake@gmail.com>
2025-04-08 17:58:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# BF 
+
+## What is this
+
+This is a small incremental project to implement the following things 
+- [x] A barebones interpreter for BrainFuck
+- [ ] A virtual machine implementation for Brainfuck (with program optimizations)
+- [ ] A compiler for Brainfuck
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod simple_interpreter;
+mod bf_executor;
+mod state;
+
+use std::{env, fs, time};
+use std::path::Path;
+use crate::bf_executor::BFExecuter;
+use crate::simple_interpreter::BFSimpleInterpreter;
+
+
+fn main() {
+
+    // generate the bf program path
+    let program_path_str = env::args().collect::<Vec<String>>()[1].clone();
+    let program_path= Path::new(&program_path_str);
+
+    let program_source = fs::read_to_string(program_path).expect("Filed to exist");
+
+    let mut bf_inter = BFSimpleInterpreter::new(program_source);
+
+    let start_time = time::Instant::now();
+
+    bf_inter.execute();
+
+    let end_time = time::Instant::now();
+
+    let program_run_time = end_time.duration_since(start_time).as_secs_f64();
+
+    println!("Instructions Evaluated {:?}", bf_inter.instruction_count());
+    println!("Time Elapsed {:?}", program_run_time);
+}
